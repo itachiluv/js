fddd1f5e7d0d80590024e618834981de0b508c2d
maniveradis@gmail.com <apple@Apples-MacBook-Air-2.local>
2024-02-23 10:38:42 +0530

    js file

---
+++ script.js
@@ -0,0 +1,58 @@
+// var a = 10
+// console.log(a)
+
+// let a = 10
+// console.log(a)
+
+// var b= 20
+// console.log(b)
+
+// if(true){
+//     var msg = "hi"
+
+// }
+// console.log(msg)
+
+var x = 10 
+let z= 45
+
+console.log(x)
+
+
+var m = 10
+var m = 20
+
+console.log(m)
+
+const ob = {
+    name:"Messi",
+    age: 34,
+    city: "Miami"
+}
+
+console.table(ob)
+
+ob.name = "Mani"
+
+console.table(ob)
+
+// type conversion
+// Number to String
+let a = 35
+console.log(a,typeof a)
+
+a = String(a)
+console.log(a,typeof a)
+
+var b= [1,2,3,4,5]
+b = b.toString()
+console.log(b,typeof b)
+
+// String to Number
+let c = "123"
+c = Number(c)
+console.log(c)
+
+d = ['mani']
+d = Number(d)  // NAN
+console.log(d) 
